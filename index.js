2c2f38ed33d5cce5dcbaca18786b7c7b635017e5
Vitor Natario <natariovitor@gmail.com>
2022-08-15 12:08:12 -0300

    feat: add route '/' and method get in '/'

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.use(express.json());
 
 //ouvindo a porta 3000
 app.listen("3000");
-console.log("Rodando");
+console.log("1... 2... 3... rodando");
+
+app.route("/").get((req, res) => {
+  res.send("RUN");
+});
