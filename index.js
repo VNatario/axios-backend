f29e289b19f3875393f95f873b4ec2a012945908
Vitor Natario <natariovitor@gmail.com>
2022-08-15 12:27:36 -0300

    feat: add axios

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 //middleware
-app.use(express.json());
+// app.use(express.json());
 
 //ouvindo a porta 3000
 app.listen("3000");
 console.log("1... 2... 3... rodando");
 
 app.route("/").get((req, res) => {
-  res.send("RUN");
+  axios
+    .get("https://api.github.com/users/VNatario")
+    .then((result) => res.send(result.data))
+    .catch((e) => console.error(e));
 });
